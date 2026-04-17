03af0e4b051253af6f5a6bbc845965a207ccb0eb
nethal17 <it23259102@my.sliit.lk>
2026-04-17 11:40:05 +0530

    feat: Add dashboard and payment summary features for clinics and platforms

--- clinic-service/app/config.py
+++ clinic-service/app/config.py
@@ -15,6 +15,7 @@ class Settings:
     AUTH_SERVICE_URL: str = os.getenv("AUTH_SERVICE_URL", "http://auth-service:8000")
     NOTIFICATION_SERVICE_URL: str = os.getenv("NOTIFICATION_SERVICE_URL", "http://notification-service:8000")
     APPOINTMENT_SERVICE_URL: str = os.getenv("APPOINTMENT_SERVICE_URL", "http://appointment-service:8000")
+    PAYMENT_SERVICE_URL: str = os.getenv("PAYMENT_SERVICE_URL", "http://payment-service:8000")
     LOGIN_URL: str = os.getenv("LOGIN_URL", "http://localhost:3000/login")
     CORS_ALLOWED_ORIGINS: str = os.getenv(
         "CORS_ALLOWED_ORIGINS",
